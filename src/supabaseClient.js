2a20f4b238ce889cf2a1594b20ed99bf4ca75543
ttvaroh <ttvaroh@icloud.com>
2025-09-21 09:59:12 -0400

    file restructuring

--- src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// ../supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-// Replace these with your actual Supabase project URL and anon key
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
